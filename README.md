f2a692fa098dc3fac492b47de2d64bbfb8e3d4c0
Miguel Mota <miguelmota2@gmail.com>
2015-05-08 00:54:20 -0700

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# audiobuffer-slice
+
+Slice out a portion of an AudioBuffer.
+
+[![NPM](https://nodei.co/npm/audiobuffer-slice.png)](https://nodei.co/npm/audiobuffer-slice)
+
+# Install
+
+```bash
+npm install audiobuffer-slice
+```
+
+```bash
+bower install audiobuffer-slice
+```
+
+# Usage
+
+See [example]() directory for full example.
+
+```javascript
+var AudioBufferSlice = require('audiobuffer-slice');
+
+// audioBuffer slice from 10s to 15s; a 5 second slice.
+AudioBufferSlice(audioBuffer, 10, 15, function(error, slicedAudioBuffer) {
+  if (error) {
+    console.error(error);
+  } else {
+    source.buffer = slicedAudioBuffer;
+  }
+});
+```
+
+# License
+
+MIT
---
+++ example/scripts/main.js
@@ -0,0 +1,88 @@
+
+(function() {
+  var url = './media/ethos-final-hope.mp3';
+  var canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+
+  var oscilloscope = AudioOscilloscope(canvas, {
+    canvas: {
+      width: function() {
+        return window.innerWidth;
+      },
+      height: 200
+    },
+    canvasContext: {
+      lineWidth: 2,
+      fillStyle: 'rgb(0,0,0)',
+      strokeStyle: 'green'
+    }
+  });
+
+  oscilloscope.draw();
+
+  var startButton = document.createElement('button');
+  startButton.textContent = 'Start';
+  startButton.disabled = true;
+  startButton.onclick = function() {
+    source.start();
+    stopped = false;
+  };
+
+  document.body.appendChild(startButton);
+
+  var stopButton = document.createElement('button');
+  var stopped = false;
+  stopButton.textContent = 'Stop';
+  stopButton.onclick = function() {
+    source.stop();
+    stopped = true;
+  };
+
+  document.body.appendChild(stopButton);
+
+  var audioContext = new AudioContext();
+  var analyser = audioContext.createAnalyser();
+  var source = audioContext.createBufferSource();
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.responseType = 'arraybuffer';
+  xhr.onerror = handleError;
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      handleBuffer(xhr.response);
+    } else {
+      console.error(xhr.statusText);
+    }
+  };
+  xhr.send();
+
+  function handleError(error) {
+    console.error(error);
+  }
+
+  function handleBuffer(audioData) {
+    audioContext.decodeAudioData(audioData, decodeDone);
+  }
+
+  function decodeDone(buffer) {
+    var begin = 50;
+    var end = begin + 20;
+
+    AudioBufferSlice(buffer, begin, end, function(error, slicedAudioBuffer) {
+      if (error) {
+        console.error(error);
+      } else {
+        source.buffer = slicedAudioBuffer;
+
+        var gainNode = audioContext.createGain();
+        gainNode.gain.value = 1;
+        source.connect(gainNode);
+        gainNode.connect(audioContext.destination);
+
+        oscilloscope.addSource(source);
+        startButton.disabled = false;
+      }
+    });
+  }
+})();
